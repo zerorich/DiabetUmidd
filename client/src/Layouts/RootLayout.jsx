6d1a98ac2ddfbbafd0593a3e09e17d25b6211738
zerorich <zerorich207@gmail.com>
2025-07-29 22:34:31 +0500

    done

--- client/src/Layouts/RootLayout.jsx
+++ client/src/Layouts/RootLayout.jsx
@@ -5,8 +5,8 @@ import Footer from "../Components/Footer";
 import logo from "../assets/DiabetUmidLogo.svg"
 import heart from "../assets/Heart.svg"
 import arrow from "../assets/arrow.svg"
-import instagram from "../assets/instagram.svg"
-import facebook from "../assets/facebook.svg"
+import instagram from "../assets/Instagram.svg"
+import facebook from "../assets/Facebook.svg"
 const RootLayout = () => {
     return (
         <>
